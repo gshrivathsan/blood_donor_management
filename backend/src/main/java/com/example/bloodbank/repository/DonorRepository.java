db8e9ffc1d66167b5966daa920d342fce910a092
SHRIVATHSAN G <82453782+gshrivathsan@users.noreply.github.com>
2026-05-15 07:35:18 +0530

    Create DonorRepository.java

---
+++ backend/src/main/java/com/example/bloodbank/repository/DonorRepository.java
@@ -0,0 +1,6 @@
+
+package com.example.bloodbank.repository;
+import com.example.bloodbank.model.Donor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DonorRepository extends JpaRepository<Donor,Long>{}
